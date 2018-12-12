2fd9f86bc99b3c1e6ca3f315939bd5eaecc484d4
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-12-12 09:09:16 -0600

    linting

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -14,8 +13,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,29 +23,29 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 /* Randomly returns one of 4 scores */
 app.post('/account/lastCreditRating', function(req, res, next) {
-  const outputs = ["good", "fair", "poor", "not found"];
-  const randNumb = Math.floor(Math.random() * 100) % 4;
-  res.send(outputs[randNumb]);
+	const outputs = ['good', 'fair', 'poor', 'not found'];
+	const randNumb = Math.floor(Math.random() * 100) % 4;
+	res.send(outputs[randNumb]);
 });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
